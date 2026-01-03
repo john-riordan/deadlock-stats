abc3241256514676920f741d581b669df37d563d
John Riordan <iam.john.riordan@gmail.com>
2026-01-03 14:37:02 -0800

    feat: related items

--- src/routes/items/[class_name]/+page.svelte
+++ src/routes/items/[class_name]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { resolve } from '$app/paths';
 	import { page } from '$app/state';
 
 	import { getItems, getVersions } from '$lib/api/static.remote';
@@ -10,21 +11,58 @@
 	});
 
 	let item = $derived(
-		items.success ? items.output.find((item) => item.class_name === page.params.class_name) : null
+		items.success
+			? items.output.find((item) => item.class_name === page.params.class_name)
+			: undefined
 	);
+
+	let relatedItems = $derived.by(() => {
+		if (!items.success || !item) return [];
+		return items.success
+			? items.output.filter(
+					(i) => i.item_tier === item.item_tier && i.item_slot_type === item.item_slot_type
+				)
+			: [];
+	});
 </script>
 
 <svelte:head>
-	<title>{item?.name} - Deadlock Item</title>
+	<title>{item?.name ?? 'Item'} - Deadlock Item</title>
 </svelte:head>
 
 {#if item}
 	<img src={item.shop_image_small_webp} alt={item.name} width={100} height={100} />
 	<h1>{item.name}</h1>
 	<span>{item.cost} Souls</span>
+	{#if relatedItems.length > 0}
+		<h2>Related Items</h2>
+		<div class="related-items">
+			{#each relatedItems.filter((item) => item.shop_image_small_webp) as relatedItem (relatedItem.id)}
+				<a href={resolve(`/items/${relatedItem.class_name}`)} class="related-item">
+					<img
+						src={relatedItem.shop_image_small_webp}
+						alt={relatedItem.name}
+						width={40}
+						height={40}
+					/>
+				</a>
+			{/each}
+		</div>
+	{/if}
 	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
 	<p>{@html item.description.desc}</p>
 	<pre>{JSON.stringify(item, null, 2)}</pre>
 {:else}
 	<h1>Item not found</h1>
 {/if}
+
+<style>
+	.related-items {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(40px, 1fr));
+		gap: 0.5rem;
+	}
+	.related-item {
+		display: block;
+	}
+</style>
